015d36f0a9d1d2d7d27fbd4c920b53cc6585258e
Jessé <jesse_ruan@hotmail.com>
2023-02-01 10:48:15 -0300

    Criados objetos a partir de url

--- src/br/com/alura/estoque/Alurator.java
+++ src/br/com/alura/estoque/Alurator.java
@@ -11,19 +11,18 @@ public class Alurator {
     public Object executa(String url) {
         // /produto/lista (método lista da class produtoController
 
-        String[] partesUrl = url.replaceFirst("/", "")
-                .split("/");
+        String[] partesUrl = url.replaceFirst("/", "").split("/");
 
-        String nomeControle = partesUrl[0] + "Controller";
+        String nomeControle = Character.toUpperCase(partesUrl[0].charAt (0)) + partesUrl[0].substring(1) + "Controller";
 
         try {
-            Class.forName(pacoteBase + nomeControle);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            Class<?> classeControle = Class.forName(pacoteBase + nomeControle);
+            Object instanciaControle = classeControle.newInstance();
+
+            System.out.println(instanciaControle);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-
-
         return null;
     }
 }
