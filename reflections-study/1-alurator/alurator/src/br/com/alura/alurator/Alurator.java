23de67b2c4b236fc937cd171c155c656fe5500a8
Jessé Ruan <98497217+jessebauer@users.noreply.github.com>
2023-02-13 09:17:13 -0300

    IoC implementada na criacao de instancia a com construtor no ContainerIoC

--- reflections-study/1-alurator/alurator/src/br/com/alura/alurator/Alurator.java
+++ reflections-study/1-alurator/alurator/src/br/com/alura/alurator/Alurator.java
@@ -4,45 +4,49 @@ import java.util.Map;
 
 import br.com.alura.alurator.conversor.ConversorXML;
 import br.com.alura.alurator.protocolo.Request;
+import br.com.alura.alurator.reflexao.ManipuladorObjeto;
 import br.com.alura.alurator.reflexao.Reflexao;
 
 public class Alurator {
-	
-	private String pacoteBase;
 
-	public Alurator(String pacoteBase) {
-		this.pacoteBase = pacoteBase;
-	}
-	
-	public Object executa(String url) {
-		// TODO - processa a requisicao executando o metodo
-		// da classe em questao
-		
-		// Produto lista
-		
-		// produto -> roduto
-		
-		Request request = new Request(url);
-		
-		String nomeControle = request.getNomeControle();
-		String nomeMetodo = request.getNomeMetodo();
-		Map<String, Object> params = request.getQueryParams();
-		
-		Object retorno = new Reflexao()
-			                .refleteClasse( pacoteBase + nomeControle )
-			                .criaInstancia()
-			                .getMetodo(nomeMetodo, params)
-			                .comTratamentoDeExcecao((metodo, ex) -> {
-				                	System.out.println("Erro no método " + metodo.getName() + " da classe " 
-				                            + metodo.getDeclaringClass().getName() + ".\n\n");
-				                	throw new RuntimeException("Erro no método!");
-			                })
-			                .invoca();
-			
-		System.out.println(retorno);
-		
-		retorno = new ConversorXML().converte(retorno);
-		
-		return retorno;
-	}
+    private String pacoteBase;
+    private ContainerIoC container;
+
+    public Alurator(String pacoteBase) {
+        this.pacoteBase = pacoteBase;
+        this.container = new ContainerIoC();
+    }
+
+    public Object executa(String url) {
+        // TODO - processa a requisicao executando o metodo
+        // da classe em questao
+
+        // Produto lista
+
+        // produto -> roduto
+
+        Request request = new Request(url);
+
+        String nomeControle = request.getNomeControle();
+        String nomeMetodo = request.getNomeMetodo();
+        Map<String, Object> params = request.getQueryParams();
+
+        Class<?> classeControle = new Reflexao().getClasse(pacoteBase + nomeControle);
+        Object instanciaControle = container.getInstancia(classeControle);
+
+        Object retorno = new ManipuladorObjeto(instanciaControle)
+                .getMetodo(nomeMetodo, params)
+                .comTratamentoDeExcecao((metodo, ex) -> {
+                    System.out.println("Erro no método " + metodo.getName() + " da classe "
+                            + metodo.getDeclaringClass().getName() + ".\n\n");
+                    throw new RuntimeException("Erro no método!");
+                })
+                .invoca();
+
+        System.out.println(retorno);
+
+        retorno = new ConversorXML().converte(retorno);
+
+        return retorno;
+    }
 }
